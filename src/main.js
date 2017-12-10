862d68f19d46bd03429494b3aec4741fd75e5a0c
Julien Gurunlian <jgurunlian@me.com>
2017-12-09 19:14:29 -0800

    Add entry file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-require('../style/app.scss')
+require('../style/app.scss');
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  const canvas = document.getElementById('canvas');
+  const GL = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
+  
+  if (!GL) {
+    alert('This browser does not support WebGL');
+  } else {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+  }
+});
